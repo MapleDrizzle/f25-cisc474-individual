888eaf213e0bffa4cbddba27a4426a1480448adb
MapleDrizzle <119966553+MapleDrizzle@users.noreply.github.com>
2025-09-12 12:18:58 -0400

    Created administrator page

---
+++ apps/web/app/administrator/page.tsx
@@ -0,0 +1,11 @@
+import link from "next/link"
+
+export default function Administrator() {
+  return (
+    <div>
+      <h1>Administrator Dashboard</h1>
+      <p>Manage course settings, user permissions, and system activity.</p>
+      <Link href="/login">Logout</Link>
+    </div>
+    );
+}
