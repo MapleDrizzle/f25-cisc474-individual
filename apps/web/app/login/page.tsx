b434871e1f256ae571c5cdcbc4a7575d05d14a71
MapleDrizzle <119966553+MapleDrizzle@users.noreply.github.com>
2025-09-15 00:06:28 -0400

    details for login page

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -2,12 +2,21 @@ import Link from "next/link"
 
 export default function LoginPage() {
   return (
-    <div>
-    <h1>Welcome! Login please:</h1>
+    <div className="max-w-sm mx-auto bg-white shadow-lg rounded-xl p-6 space-y-4">
+      <div className="text-center">
+        <div className="text-4x1">🍞</div>
+           <h1>Welcome! Login please:</h1>
+        </div>
+      <input type="text" placeholder="Username" className="w-full border rounded p-2" />
+      <input type="password" placeholder="Password" className="w-full border rounded p-2" />
+      <button
+        onClick={() => handleLogin("student")}
+        className="w-full bg-blue-600 text-white rounded p-2">Login
+      </button>
       <ul>
-      <li><Link href="/student">Student Dashboard</Link></li>
-      <li><Link href="/instructor">Instructor Dashboard</Link></li>
-      <li><Link href="/administrator">Administrator Dashboard</Link></li>
+        <li><Link href="/student">Student Dashboard</Link></li>
+        <li><Link href="/instructor">Instructor Dashboard</Link></li>
+        <li><Link href="/administrator">Administrator Dashboard</Link></li>
       </ul>
     </div>
   );
