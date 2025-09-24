147c38b177fb754c0d136571e68535b7e2a5b2a2
MapleDrizzle <siromero@udel.edu>
2025-09-24 15:22:44 -0400

    trying to fix prisma error

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -2,9 +2,10 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
+export const prisma = new PrismaClient();
+/*
   globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export * from "@prisma/client";
+*/
+export * from "../generated/client";
