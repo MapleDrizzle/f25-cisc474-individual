8c3a04de5fa336924385af3c61283e9d751f6098
MapleDrizzle <siromero@udel.edu>
2025-09-29 22:32:08 -0400

    fixing connection issues

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,12 +5,10 @@ import { LinksModule } from './links/links.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { UsersModule } from './users.module';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 
 @Module({
   imports: [LinksModule, UsersModule],
-  controllers: [AppController, UsersService],
-  providers: [AppService, UsersController],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- apps/api/src/users.controller.ts
+++ apps/api/src/users.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 
-@Controller('user')
+@Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
