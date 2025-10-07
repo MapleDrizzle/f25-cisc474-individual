47e7e1390680c97238ea71f1ee84798d7bb8c175
MapleDrizzle <siromero@udel.edu>
2025-10-06 20:47:12 -0400

    fixing CORS error

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,14 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
-  const host = process.env.HOST || undefined;
-  await app.listen(port, host);
-
   app.enableCors({
     origin: [process.env.FRONTEND_URL],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
+  
+  const port = process.env.PORT || 3000;
+  const host = process.env.HOST || undefined;
+  await app.listen(port, host);
 }
 void bootstrap();
